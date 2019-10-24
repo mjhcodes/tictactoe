8b3b168c5fb221c8b83e843ce6a49b19c8329cdb
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-23 18:47:57 -0700

    created basic Player/Game classes

---
+++ tictactoe.py
@@ -0,0 +1,27 @@
+# Tic Tac Toe
+
+class Player:
+  def __init__(self, name, token):
+    self.name = name
+    self.token = token
+
+class Game:
+  def __init__(self, board):
+    self.board = board
+
+  def __repr__(self):
+    pass
+
+  def move(self, x, y, player):
+    pass
+
+  def calc_winner(self):
+    pass
+
+  def is_full(self):
+    pass
+
+  def is_game_over(self):
+    pass
+
+

abb8b1a617bff2b7b328d1b68248d308741754c5
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-24 19:49:46 -0700

    fixed ValueError and repeat location bugs

--- tictactoe.py
+++ tictactoe.py
@@ -20,39 +20,42 @@ class Game:
   def move(self, x, y, token):
     """checks the requested space on the board; if taken, returns message; if available, places player token at specified coordinates"""
     if self.board[x][y] != " ":
-      return "Space already taken. Please try again."
+      print("\nSpace already taken. Please try again...\n")
+      return True
     else:
       self.board[x][y] = token
 
-  # def calc_winner(self):
-  #   match = 0
-  #   for x in range(3):
-  #     if self.board[x][y] == self.board[x][y]:
-  #       match = match + 1
-  #       if match == 3:
-  #         return True
-  
-
+  def calc_winner(self):
+    # match = 0
+    # for x in range(3):
+    #   if self.board[x][y] == self.board[x][y]:
+    #     match = match + 1
+    #     if match == 3:
+    #       return True
+    pass
+      
   def is_full(self):
-    """scans each row of the board and returns False, if any section is blank; otherwise, returns True"""
+    """scans each row of the board and returns False, if any section is blank; otherwise, prints message and quits"""
     for row in self.board:
       if any(item == " " for item in row):
         return False
     print("No more moves available. Game over.\n")
     quit()
 
-  # def is_game_over(self):
-  #   """runs two methods to determine if game has concluded"""
-  #   return self.calc_winner() or self.is_full()
+  def is_game_over(self):
+    """runs two methods to determine if game has concluded"""
+    return self.calc_winner() or self.is_full()
 
 
 # - - - - - - - END CLASSES - - - - - - - -
+
 
 def get_current_player(current_round, player_one, player_two):
   if current_round % 2 == 0:
     return player_two
   else:
     return player_one
+
 
 def main():
   board = Game()
@@ -70,21 +73,27 @@ def main():
 
   while True:
 
+    board.is_game_over()
+
     current_player = get_current_player(current_round, player_one, player_two)
 
-    board.is_game_over()
-    x = int(input(f"{current_player.name}, select your horizontal location (0-2): "))
-    y = int(input(f"Select your vertical location (0-2): "))
+    try:
+      x = int(input(f"{current_player.name}, select your horizontal location (0-2): "))
+      y = int(input(f"Select your vertical location (0-2): "))
+    except ValueError:
+      print("\nNot a valid input. Please try again...\n")
+      continue
+
     if 0 <= x <= 2 and 0 <= y <= 2:
-      board.move(x, y, current_player.token)
+      is_taken = board.move(y, x, current_player.token)
+      if is_taken == True:
+        continue
       print(board)
     else:
-      print("\nNot a valid move. Please try again.\n")
+      print("\nNot a valid move. Please try again...\n")
       continue
 
     current_round += 1
-
-
 
 
 main()
